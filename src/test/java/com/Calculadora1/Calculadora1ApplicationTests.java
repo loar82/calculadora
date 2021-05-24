23a2dfa7dfb487bd81dc1cb401831a9466d772cc
Lenovo <Lenovo@Addi-R>
2021-05-24 17:28:50 -0500

    creacion

---
+++ src/test/java/com/Calculadora1/Calculadora1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Calculadora1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calculadora1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
